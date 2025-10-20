fcfde894cd4782e290accd5f98e8e9f764b92d63
Yago Puente <yagopuentetecnologia@gmail.com>
2025-10-20 19:15:58 +0200

    commit de la calculadora inicial

---
+++ calculadora.py
@@ -0,0 +1,24 @@
+opcion=input("""Dime que opcion quieres
+             1- Sumar
+             2- Restar
+             3-Multiplicar
+             4-Dividir
+             5-Salir""")
+
+match opcion:
+    case 1:
+        pass
+    case 2:
+        # xxxx
+        pass
+    case 3:
+        pass
+        # xxxxx
+    case 4:
+        pass
+        # xxxxx
+    case 5:
+        pass
+        # xxxx
+    case _:
+        pass
